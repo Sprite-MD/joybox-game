1376eee5ab49dcc38c364bb3c801cb25252cf42f
Sprite <ponkaew.wasin@gmail.com>
2024-04-06 21:38:37 -0500

    new files

---
+++ app.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask import Flask, g
+
+DATABASE =  'database.db'
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        
+if __name__ == "__main__":
+    app.run()
---
+++ db.py
@@ -0,0 +1,9 @@
+import sqlite3
+from flask import Flask, g
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
